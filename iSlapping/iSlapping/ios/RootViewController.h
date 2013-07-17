3760c44e6742f44bff4b67bf65ee5166e2e82c99
Zhenyu <citydeer@gmail.com>
2013-07-18 00:17:56 +0800

    init project

---
+++ iSlapping/iSlapping/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  iSlappingAppController.h
+//  iSlapping
+//
+//  Created by Pang Zhenyu on 13-7-17.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
